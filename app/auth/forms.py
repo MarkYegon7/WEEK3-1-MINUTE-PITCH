99bb6712023148d58397774abd3bf8943aeb1a0b
MarkYegon7 <mark.yegon@student.moringaschool.com>
2022-05-20 17:34:53 +0300

    ft:created loginform

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,11 +4,21 @@ from wtforms.validators import InputRequired,Email,EqualTo
 from wtforms import ValidationError
 from ..models import User
 
+class RegistrationForm(FlaskForm):
+
+    email = StringField('Your Email Address',validators=[InputRequired(),Email()])
+    username = StringField('Enter your username',validators= [InputRequired()])
+    password = PasswordField('Password',validators = [InputRequired(),EqualTo('password_confirm',message = 'Password must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
 
 
 
-class LoginForm(FlaskForm):
-    email = StringField('Your Email Address',validators=[InputRequired()])
-    password = PasswordField('Password',validators=[InputRequired()])
-    remember = BooleanField('Remember me')
-    submit = SubmitField('Sign In')
